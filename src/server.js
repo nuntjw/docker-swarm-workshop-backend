054bf3bb1e6e174318e1da7c4899f7df273a6f27
Nan <nuntjw@gmail.com>
2021-03-06 09:21:43 +0700

    create project

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import router from "./app/routes";
+
+const app = express();
+
+app
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(router);
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`server running on ${process.env.PORT}`);
+});
