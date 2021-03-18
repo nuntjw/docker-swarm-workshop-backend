144fbe775c8896e095c2002bf724b673ab9779db
Nan <nuntjw@gmail.com>
2021-03-18 14:20:41 +0700

    commit log hello

--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ import bodyParser from "body-parser";
 import router from "./app/routes";
 
 const app = express();
+console.log('hello')
 
 app
   .use(cors())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(router);
+
 
 
 app.listen(process.env.PORT, () => {
